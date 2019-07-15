3535efb46d980f5acf507f2ff96aad670cbb3e3c
Andy Cheng <b10402113@gmail.com>
2019-07-15 14:12:59 +0800

    Project01

---
+++ 01_PhotoGraphyProject/Photography_project/Photography_project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Photography_project
+//
+//  Created by 鄭和軒 on 2019/7/9.
+//  Copyright © 2019年 鄭和軒. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
